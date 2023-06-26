f5dd1c18890dbec0d5d079ce210f64a9aed1aede
Pratham1643 <79498015+Pratham43@users.noreply.github.com>
2023-06-26 18:34:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, jsonify
-from flask_restful import Api, Resource, abort 
+from fastapi import FastAPI, HTTPException
 
-app = Flask(__name__)
-api = Api(app)
-
-class CodechefUser(Resource):
-    def get(self, username):
-        res = requests.get(f"https://www.codechef.com/users/{username}")
-        if res.status_code != 200:
-            abort(404, message="User Not Found")
-
-        soup = BeautifulSoup(res.text, 'html.parser')
-        rating = self.get_rating(soup)
-        problems_solved = self.get_problems_solved(soup)
-
-        return jsonify({
-            "username": username,
-            "rating": rating,
-            "problemsSolved": problems_solved
-        })
-
-    def get_rating(self, soup):
-        rating_div = soup.find_all(["div"], class_="rating-number")
-        if len(rating_div) == 0:
-            abort(404, message="User Not Found")
-        return rating_div[0].text
-
-    def get_problems_solved(self, soup):
-        sections = soup.find(["section"], class_="rating-data-section problems-solved")
-        if sections is None:
-            abort(404, message="User Not Found")
-        problem_solved_sections = sections.h5.text
-        probs_solved = problem_solved_sections.split("(")
-        probs_solved = probs_solved[1].split(")")[0]
-        return probs_solved
-
-class CodeforcesUser(Resource):
-    def get(self, username):
-        url = f"https://codeforces.com/api/user.info?handles={username}"
-        res = requests.get(url)
-        if res.status_code != 200:
-            abort(404, message="User Not Found")
-
-        data = res.json()
-        if "result" not in data or len(data["result"]) == 0:
-            abort(404, message="User Not Found")
-
-        rating = data["result"][0].get("rating")
-        codeForcesProblemsSolved = self.get_problems_solved(username)
-        return jsonify({
-            "username": username,
-            "rating": rating,
-            "problemsSolved": codeForcesProblemsSolved
-        })
-    
-    def get_problems_solved(self,username):
-        forces_res = requests.get(f"https://codeforces.com/profile/{username}")
-        if forces_res.status_code != 200:
-            abort(404, message="User Not Found")
-        forces_soup = BeautifulSoup(forces_res.text, 'html.parser')
-        problems_div = forces_soup.find_all(["div"], class_="_UserActivityFrame_counterValue")
-        if len(problems_div) == 0:
-            abort(404, message="User Not Found")
-        return problems_div[0].text.split()[0]
+app = FastAPI()
 
 
-api.add_resource(CodechefUser, '/codechef/<username>')
-api.add_resource(CodeforcesUser, '/codeforces/<username>')
+@app.get("/codechef/{username}")
+async def get_codechef_user(username: str):
+    res = requests.get(f"https://www.codechef.com/users/{username}")
+    if res.status_code != 200:
+        raise HTTPException(status_code=404, detail="User Not Found")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    rating = get_rating(soup)
+    problems_solved = get_problems_solved(soup)
+
+    return {
+        "username": username,
+        "rating": rating,
+        "problemsSolved": problems_solved
+    }
+
+
+@app.get("/codeforces/{username}")
+async def get_codeforces_user(username: str):
+    url = f"https://codeforces.com/api/user.info?handles={username}"
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise HTTPException(status_code=404, detail="User Not Found")
+
+    data = res.json()
+    if "result" not in data or len(data["result"]) == 0:
+        raise HTTPException(status_code=404, detail="User Not Found")
+
+    rating = data["result"][0].get("rating")
+    problems_solved = get_codeforces_problems_solved(username)
+
+    return {
+        "username": username,
+        "rating": rating,
+        "problemsSolved": problems_solved
+    }
+
+
+def get_rating(soup):
+    rating_div = soup.find_all("div", class_="rating-number")
+    if len(rating_div) == 0:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    return rating_div[0].text
+
+
+def get_problems_solved(soup):
+    sections = soup.find("section", class_="rating-data-section problems-solved")
+    if sections is None:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    problem_solved_sections = sections.h5.text
+    problems_solved = problem_solved_sections.split("(")[1].split(")")[0]
+    return problems_solved
+
+
+def get_codeforces_problems_solved(username):
+    forces_res = requests.get(f"https://codeforces.com/profile/{username}")
+    if forces_res.status_code != 200:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    forces_soup = BeautifulSoup(forces_res.text, 'html.parser')
+    problems_div = forces_soup.find_all("div", class_="_UserActivityFrame_counterValue")
+    if len(problems_div) == 0:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    return problems_div[0].text.split()[0]
